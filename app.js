e71c20801fa10795e957c4a959de9a7e8fd80bbc
dviiiii <913284195@qq.com>
2018-11-16 23:43:25 +0800

    新增koa-json

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
+const json = require('koa-json');
 const router = require('./router');
 const bodyParser = require('koa-bodyparser');
 
+
 app.use(bodyParser());
+app.use(json());
 
 app
     .use(router.routes())
